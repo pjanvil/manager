792191bb352ef57c08f0c96dd7a8f02886939db9
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-11-26 18:06:55 +0000

    sign in on firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
-
+import ReduxThunk from 'redux-thunk';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
@@ -25,7 +24,7 @@ class App extends Component {
 
     render() {
         return (
-            <Provider store={createStore(reducers)}>
+            <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
                 <LoginForm />
             </Provider>
         );
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED
@@ -15,4 +17,13 @@ export const passwordChanged = (text) => {
         type: PASSWORD_CHANGED,
         payload: text
     }
+}
+
+export const loginUser = ({ email, password }) => {
+    return (dispatch) => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => {
+                dispatch({type: 'LOGIN_USER_SUCCESS', payload: user })
+            });
+    };
 }
