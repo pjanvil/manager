df54ec08ca5590edb056332d902985db60588b0b
Paul Jones <pjanvil@gmail.com>
2019-01-10 17:10:09 +0000

    lec145

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_CREATE,
-    EMPLOYEE_UPDATE
+    EMPLOYEE_UPDATE,
+    EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ( { prop, value } ) => {
@@ -20,6 +21,17 @@ export const employeeCreate = ({name, phone, shift}) => {
             .then(() => {
             dispatch({type: EMPLOYEE_CREATE });
             Actions.employeeList({ type: 'reset' })
+            });
+    };
+}
+
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                // snapshot is an object not an array
+                dispatch({type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
             });
     };
 }
